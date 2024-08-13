b380c298a05eb387d61672ce7333b6da6ee89147
Chris Mague <github@mague.com>
2024-08-13 08:47:42 -0700

    basic ddl

--- PropertyGraphDDL.sql
+++ PropertyGraphDDL.sql
@@ -1,41 +1,47 @@
+-- Node Tables
 CREATE TABLE Owner (
   id               INT64 NOT NULL,
   create_time      TIMESTAMP,
   name             STRING(MAX),
+  ssn             STRING(MAX),
 ) PRIMARY KEY (id);
 
 CREATE TABLE Property (
   id               INT64 NOT NULL,
   create_time      TIMESTAMP,
+  street_address   STRING(MAX),
 ) PRIMARY KEY (id);
 
-CREATE TABLE Insurer (
+-- CREATE TABLE Insurer (
+--   id               INT64 NOT NULL,
+--   create_time      TIMESTAMP,
+--   policy           STRING(MAX),
+-- ) PRIMARY KEY (id);
+-- 
+-- CREATE TABLE County (
+--   id               INT64 NOT NULL,
+-- ) PRIMARY KEY (id);
+-- 
+-- CREATE TABLE CreditBureau (
+--   id               INT64 NOT NULL,
+--   create_time      TIMESTAMP,
+--   name             STRING(MAX),
+-- ) PRIMARY KEY (id);
+
+-- Edge Tables
+
+CREATE TABLE OwnerOwnsProperty (
   id               INT64 NOT NULL,
-  account_id       INT64 NOT NULL,
+  property_id       INT64 NOT NULL,
   create_time      TIMESTAMP,
-  FOREIGN KEY (account_id) REFERENCES Owner (id)
-) PRIMARY KEY (id, account_id),
-  INTERLEAVE IN PARENT Property ON DELETE CASCADE;
+  FOREIGN KEY (property_id) REFERENCES Property (id)
+) PRIMARY KEY (id, property_id);
 
-#CREATE TABLE AccountTransferAccount (
-#  id               INT64 NOT NULL,
-#  to_id            INT64 NOT NULL,
-#  amount           FLOAT64,
-#  create_time      TIMESTAMP NOT NULL,
-#  order_number     STRING(MAX),
-#  FOREIGN KEY (to_id) REFERENCES Account (id)
-#) PRIMARY KEY (id, to_id, create_time),
-#  INTERLEAVE IN PARENT Account ON DELETE CASCADE;
-
-CREATE OR REPLACE PROPERTY GRAPH FinGraph
-  NODE TABLES (Owner, Propery, Insurer)
+CREATE OR REPLACE PROPERTY GRAPH PropertyGraph 
+  NODE TABLES (Owner, Property)
   EDGE TABLES (
-    PersonOwnProperty
+    OwnerOwnsProperty
       SOURCE KEY (id) REFERENCES Owner (id)
-      DESTINATION KEY (account_id) REFERENCES Property (id)
-      LABEL Owns,
-    InsuresProperty
-      SOURCE KEY (id) REFERENCES Insurer (id)
-      DESTINATION KEY (to_id) REFERENCES Property (id)
-      LABEL Transfers
+      DESTINATION KEY (property_id) REFERENCES Property (id)
+      LABEL OWNS
   );
