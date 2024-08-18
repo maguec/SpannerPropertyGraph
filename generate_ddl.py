e0ca8b6e41c2e6af22458016f11d94029406d2d6
Chris Mague <github@mague.com>
2024-08-18 09:48:42 -0700

    formatted DDL output

--- generate_ddl.py
+++ generate_ddl.py
@@ -5,7 +5,7 @@ from models.edges import *
 from models.utils import *
 
 if __name__ == "__main__":
-    full_ddl = []
+    full_ddl = ["-- Node Tables"]
     counties = Counties()
     full_ddl.append(counties.genddl())  # counties.genddl()
     properties = Properties()
@@ -14,15 +14,32 @@ if __name__ == "__main__":
     full_ddl.append(owners.genddl())  # owners.genddl()
     credit_reports = CreditReports()
     full_ddl.append(credit_reports.genddl())  # credit_reports.genddl()
+    full_ddl.append("-- Edge Tables")
     credit_edges = CreditEdges(owners, credit_reports)
     full_ddl.append(credit_edges.genddl())  # credit_edges.genddl()
     county_edges = CountyEdges(properties, counties)
     full_ddl.append(county_edges.genddl())  # county_edges.genddl()
     property_edges = PropertyEdges(properties, owners)
     full_ddl.append(property_edges.genddl())  # property_edges.genddl()
-    full_ddl.append(property_edges.gendeclarationddl())
-    full_ddl.append(county_edges.gendeclarationddl())
-    full_ddl.append(credit_edges.gendeclarationddl())
-    
+    full_ddl.append("-- Graph Declaration")
+    full_ddl.append("  NODE TABLES (")
+    full_ddl.append(
+        "    "
+        + ",\n    ".join(
+            [
+                counties.list_items[0].__class__.__name__,
+                properties.list_items[0].__class__.__name__,
+                owners.list_items[0].__class__.__name__,
+                credit_reports.list_items[0].__class__.__name__,
+            ]
+        )
+    )
+    full_ddl.append("  )")
+    full_ddl.append("  EDGE TABLES (")
+    full_ddl.append("    " + property_edges.gendeclarationddl())
+    full_ddl.append("    " + county_edges.gendeclarationddl())
+    full_ddl.append("    " + credit_edges.gendeclarationddl())
+    full_ddl.append("  )")
+    full_ddl.append(");")
 
     print("\n".join(full_ddl))
